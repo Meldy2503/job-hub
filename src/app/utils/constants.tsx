a4d1ec432fe559b0a082f19814d1765e9517765e
meldy2503 <emelder.charles25@gmail.com>
2024-03-10 02:36:02 +0100

    feat: added the footer component

--- src/app/utils/constants.tsx
+++ src/app/utils/constants.tsx
@@ -1,3 +1,7 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
+
+
 export const navData = [
     {
         id: "1",
@@ -18,3 +22,48 @@ export const navData = [
     },
   ];
   
+
+  export const footerData = [
+   
+      {
+        icon: <MdOutlineEmail />,
+        text: "hello@mycompany.com",
+      },
+    {
+      icon: <FiPhoneCall />,
+      text: "+1 (650) 555-0111",
+    },
+  ];
+
+
+  
+  export const footerNavLinksA = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "Jobs",
+      link: "/pages/jobs",
+    },
+    {
+      title: "About us",
+      link: "/pages/blogs",
+    },
+   
+  ];
+  export const footerNavLinksB = [
+    
+    {
+      title: "Get Started",
+      link: "/pages/auth/register",
+    },
+    {
+      title: "Terms of Services",
+      link: "#",
+    },
+    {
+      title: "Privacy Policy",
+      link: "#",
+    },
+  ];
